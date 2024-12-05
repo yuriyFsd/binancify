bac658f991908f5e7d2e2b031e457ff50e1df84e
Yuriy O <yuriy1@yahoo.com>
2024-12-05 22:27:04 +0200

    WIP

--- binancify.js
+++ binancify.js
@@ -7,9 +7,10 @@ test1()
 function test1() {
     //beep()    
     console.log("start")    
+    extractColumnValues("")
     //for (let i = 0; i < 30; i++) {
         setInterval(() => {
-           beep(20) 
+           //beep(20) 
         }, 2000);
         // setTimeout(() => {
         //     beep(i)
@@ -51,3 +52,27 @@ function tableToArray(tableId) {
     // Example usage: 
 // Assume your table has an ID of "myTable" 
 let tableArray = tableToArray("myTable"); console.log
+
+//class="AdvTableList"
+//let tbl = document.getElementsByClassName("AdvTableList); 
+//container relative bg-backgroundBasic
+
+function extractColumnValues(className = "bn-web-table-container", columnIndex = 2) {
+    
+    console.log('extractColumnValues')
+    let myrows = document.getElementsByClassName("bn-web-table-row bn-web-table-row-level-0") 
+    console.log(myrows[0].cells[1].innerText)
+    return
+    //document.body.style.border = "5px solid green";
+    const divElems = document.getElementsByClassName(className)
+    console.log( {res: divElems[0]} )
+    var columnValues = []; 
+     for (var i = 0; i < divElem.length; i++) { 
+        var divElem = divElems[i];
+        console.log(typeof divElem);
+        // for (var i = 1, row; row = table.rows[i]; i++) {
+        //     var cellValue = row.cells[columnIndex].innerText; columnValues.push(cellValue); 
+        // } 
+     }
+    return columnValues; 
+}
